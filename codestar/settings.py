5a7d37e438fa0825b9e875590b71da6f46863ab3
Graciete Gil <graciete.gil@gmail.com>
2024-02-29 10:28:59 +0000

    Deployment settings

--- codestar/settings.py
+++ codestar/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +29,7 @@ SECRET_KEY = 'django-insecure-cue^+^rd0el4$(sdwx3&r@^5kn9#_o$$vdc8k$i^mf(n+n9(l8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://vm5qkj1v-8000.uks1.devtunnels.ms/blog/', '.herokuapp.com', 'http://127.0.0.1:8000/', '127.0.0.1', 'localhost', 'yourdomain.com', 'www.yourdomain.com',]
+ALLOWED_HOSTS = ['https://vm5qkj1v-8000.uks1.devtunnels.ms/blog/', '.herokuapp.com', 'http://127.0.0.1:8000/', '127.0.0.1', 'localhost',  'https://blog-app-graciete-94fe55f6b7e6.herokuapp.com/']
 
 
 # Application definition
@@ -74,11 +78,16 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#       'ENGINE': 'django.db.backends.sqlite3',
+#       'NAME': BASE_DIR / 'db.sqlite3',
+#   }
+#}
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
 
