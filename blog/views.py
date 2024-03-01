49d965c85d3f301d3a6c0c490998314fdda52cd5
Graciete Gil <graciete.gil@gmail.com>
2024-03-01 23:59:47 +0000

    Add a new filter to return only  posts with status of Published

--- blog/views.py
+++ blog/views.py
@@ -6,5 +6,6 @@ from .models import Post
 # Create your views here.
 
 class PostList(generic.ListView):
-    queryset = Post.objects.all()
+    queryset = Post.objects.filter(status=1)
     template_name = "post_list.html"
+#This means that we can now leave some posts in Draft while we finish them, and they will not show up on the live blog.
